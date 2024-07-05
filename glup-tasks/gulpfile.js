f62113848785e4d136574cdb3d929b7d294de322
Anjali <anjalijanar@gmail.com>
2024-07-05 13:26:48 +0530

    added gulp tasks

--- glup-tasks/gulpfile.js
+++ glup-tasks/gulpfile.js
@@ -1,6 +1,10 @@
 
-const gulp = require("gulp");
+import gulp from 'gulp';
+import imagemin from 'gulp-imagemin';
 
-gulp.task('imagemin', () => {
+gulp.task('default', () => {
     // define task
+    return gulp.src("src/images/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("dest/images"))
 });
